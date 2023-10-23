34cf992feb1d0369ae5c3fdb1e8e3a52071cb6e3
mimi5963 <mimi5963@naver.com>
2023-10-23 21:05:54 +0900

    servlet

---
+++ webtest2/src/main/java/test/Test2.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+ 
+@WebServlet("/Test2")
+public class Test2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public Test2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("<h1>test<h1>");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
